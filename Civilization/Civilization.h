184aa940197a5f3ab3ddcf3fb79e71c66d51b3ef
Sergey Firsov <fearthunder@yandex.ru>
2022-11-12 19:34:04 +0300

    FIx: Civilization border now has positive default value

--- Civilization/Civilization.h
+++ Civilization/Civilization.h
@@ -10,7 +10,7 @@ struct Color
 struct Civilization
 {
     unsigned int Age = 0;
-    unsigned int BorderLeft = -1;
-    unsigned int BorderRight = -1;
+    unsigned int BorderLeft = 0;
+    unsigned int BorderRight = 0;
     struct Color Color;
 };
