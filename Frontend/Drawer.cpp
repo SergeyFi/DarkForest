c7e08da0cdc60467268ccf6ab25488dc890c7162
Sergey Firsov <fearthunder@yandex.ru>
2022-11-16 22:55:41 +0300

    Feature: add step render

--- Frontend/Drawer.cpp
+++ Frontend/Drawer.cpp
@@ -27,28 +27,30 @@ void Drawer::StartDrawLoop()
 
 void Drawer::DrawUniverse(sf::RenderWindow& Window)
 {
-    sf::Image Image;
-    Image.create(Universe->GetSize(), 1, sf::Color::Black);
+    int RenderStep = std::sqrt(Universe->GetSize() / 16) / 2;
 
-    for (auto i = 0; i < Universe->GetSize(); ++i)
+    sf::Image Image;
+    Image.create(Universe->GetSize() / RenderStep, 1, sf::Color::Black);
+
+    for (auto i = 0; i < Universe->GetSize(); i += RenderStep)
     {
         if (Universe->GetVoids()[i].Civilization == nullptr)
         {
-            Image.setPixel(i, 0, {130, 170, 255});
+            Image.setPixel(i / RenderStep, 0, {130, 170, 255});
         }
         else
         {
             auto CivColor = Universe->GetVoids()[i].Civilization->Color;
-            Image.setPixel(i, 0, {CivColor.Red, CivColor.Green, CivColor.Blue});
+            Image.setPixel(i / RenderStep, 0, {CivColor.Red, CivColor.Green, CivColor.Blue});
         }
     }
 
-    for (auto i = 0; i < std::ceil(Universe->GetSize() / float(TextureSizeMax)); ++i)
+    for (auto i = 0; i < std::ceil((Universe->GetSize() / RenderStep) / float(TextureSizeMax)); ++i)
     {
         sf::Texture Texture;
         Texture.loadFromImage(Image, {i * TextureSizeMax, 0, TextureSizeMax, 1});
 
-        auto VoidSize = float(Window.getSize().x) / float(Universe->GetSize());
+        auto VoidSize = float(Window.getSize().x) / float(Universe->GetSize() / RenderStep);
 
         sf::Sprite Sprite;
         Sprite.setTexture(Texture);
